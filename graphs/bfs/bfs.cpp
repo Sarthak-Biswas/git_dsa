e90323c77f2e719086d6c58ede223cf4d64cf465
Sarthak Biswas <sarthakb500@gmail.com>
2022-12-01 01:01:22 +0530

    12/1/22

--- graphs/bfs/bfs.cpp
+++ graphs/bfs/bfs.cpp
@@ -6,10 +6,30 @@ using namespace std;
 #define dbgline cerr << endl;
 
 unordered_map<int, vector<int>> g;
+vector<int> vis(1000,false);
 
 void bfs(int u)
 {
+    list<int> q;
+    q.push_back(u);
+    vis[u] = true;
 
+    while(!q.empty())
+    {
+        int v = q.front();
+        q.pop_front();
+
+        cout<<v<<", ";
+
+        for(auto i : g[v])
+        {
+            if(!vis[i])
+            {
+                q.push_back(i);
+                vis[i] = true;
+            }
+        }
+    }
 }
 
 
@@ -38,9 +58,6 @@ int main()
         g[y].push_back(x);
     }
 
-    for(auto i : g)
-    {
-        cout<<i.first<<endl;
-    }
+    bfs(g.begin()->first);
     return 0;
 }
